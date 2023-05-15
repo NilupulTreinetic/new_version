9d1c7206aa64f350ec118aade3b9392fc26d544c
NilupulTreinetic <nilupul@treinetic.com>
2023-05-15 10:22:16 +0530

    android url changes

--- lib/new_version.dart
+++ lib/new_version.dart
@@ -151,8 +151,10 @@ class NewVersion {
   Future<VersionStatus?> _getAndroidStoreVersion(
       PackageInfo packageInfo) async {
     final id = androidId ?? packageInfo.packageName;
-    final uri = Uri.https(
-        "play.google.com", "/store/apps/details", {"id": "$id", "hl": "en"});
+
+// final uri = Uri.https(
+//         "play.google.com", "/store/apps/details", {"id": "$id", "hl": "en"});
+    final uri = "market://details?id=$id";
     final response = await http.get(uri);
     if (response.statusCode != 200) {
       debugPrint('Can\'t find an app in the Play Store with the id: $id');
